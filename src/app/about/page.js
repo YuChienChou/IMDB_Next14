ec5cf87393724dc888ff225ad858b7df3f136e9b
Leslie Chou <chouyuchien@gmail.com>
2024-04-04 21:20:57 -0700

    added about page

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+
+    <div className='max-w-6xl p-3 mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>p section</p>
+      <p>p section</p>
+      <p>p section</p>
+    </div>
+
+    
+  )
+}
